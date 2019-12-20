87f1da86be53f58143e904072649fc7446d03089
Thanh Cao <songnuoclantan@gmail.com>
2019-12-20 14:55:11 +0700

    UI LOGIN AND REGISTER

---
+++ call_car_project/lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:call_car_project/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
---
+++ call_car_project/lib/src/resources/login_page.dart
@@ -0,0 +1,120 @@
+import 'package:call_car_project/src/resources/register_page.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 140, 0, 0),
+                child: Image.asset('ic_car_green.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 6),
+                child: Text(
+                  "Welcome back!",
+                  style: TextStyle(fontSize: 22, color: Color(0xff333333)),
+                ),
+              ),
+              Text(
+                "Login to continue using iCab",
+                style: TextStyle(fontSize: 16, color: Color(0xff606470)),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 110, 0, 20),
+                child: TextField(
+                  controller: _emailController,
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  decoration: InputDecoration(
+                      labelText: "Email",
+                      prefixIcon: Container(
+                          width: 50, child: Image.asset("ic_mail.png")),
+                      border: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: Color(0xffCED0D2), width: 1),
+                          borderRadius: BorderRadius.all(Radius.circular(12)))),
+                ),
+              ),
+              TextField(
+                controller: _passController,
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                obscureText: true,
+                decoration: InputDecoration(
+                    labelText: "Password",
+                    prefixIcon: Container(
+                        width: 50, child: Image.asset("ic_phone.png")),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffCED0D2), width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(12)))
+                ),
+              ),
+              Container(
+                constraints: BoxConstraints.loose(Size(double.infinity, 30)),
+                alignment: AlignmentDirectional.centerEnd,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Text(
+                    "Forgot password?",
+                    style: TextStyle(fontSize: 16, color: Color(0xff606470)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 40),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 52,
+                  child: RaisedButton(
+                    onPressed: null,
+                    child: Text(
+                      "Log In",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                    color: Color(0xff3277D8),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: RichText(
+                  text: TextSpan(
+                      text: "New user? ",
+                      style: TextStyle(color: Color(0xff606470), fontSize: 16),
+                      children: <TextSpan>[
+                        TextSpan(
+                            recognizer: TapGestureRecognizer()..onTap = () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> RegisterPage()));
+                            },
+                            text: "Sign up for a new account",
+                            style: TextStyle(
+                                color: Color(0xff3277D8), fontSize: 16))
+                      ]),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
